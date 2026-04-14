4a2ac513a434b0c06d32be5152948d50eabc3912
Codelossus <isotelo@megacable.com.mx>
2026-04-14 09:32:36 -0600

    No mames no peguen sin leer se chispo todo pero ya quedó alv JAJAJA

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DeepFocus/', // <-- Línea CRUCIAL: el nombre de tu repositorio
   server: { host: true },
 });
